cfc0ec015466e7e075be70f90b46b32a620c2375
RagnarSparda <129216900+RagnarSparda@users.noreply.github.com>
2023-04-06 16:02:49 -0400

    Add files via upload

---
+++ 02-tipos de datos.py
@@ -0,0 +1,28 @@
+#01-DATOS DE TIPO NUMERICO}
+estatura = 1.71
+peso = 70
+conplejo = 1 -4j
+
+print("Impresion del numero complejo:",conplejo)
+#OPERACIO ARITMETICA BÁSICA
+inc = peso/estatura**2
+print(inc)
+
+#02- DATOS DE TIPO CADENA DE CARACTERES
+asignatura = "Programacion"
+carrera = "Ingenieria Civil en Informatica"
+
+print("La asignatura de",asignatura,"corresponde a la carrera de",carrera)
+
+#03- DATOS DE TIPO BOOLEAMOS
+ampolleta = False
+iterruptor = True
+
+#Con-type savemos el tipo de datos que estamos tratando
+print(type(ampolleta))
+
+#04- DATOS TIPO ARRAY (Objetos de Tipo Colección)
+estudiantes = ["Matias", "Marcos", "Cristobal", "Sebastian"]
+num = [1,2,3,4,5,6]
+print(estudiantes)
+print(num)

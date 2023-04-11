cfc04626ab9018237c1898be23167202fbd5c631
RagnarSparda <129216900+RagnarSparda@users.noreply.github.com>
2023-04-11 15:23:04 -0400

    Add files via upload

---
+++ 01-variable.py
@@ -0,0 +1,31 @@
+#Un simple print
+print("hola soy Jeremías")
+
+#Declarando una variable
+nombre = "Jeremías"
+name = "Ignacio"
+
+#Imprecion de un Nombre
+print(nombre)
+print("Hola soy",nombre)
+
+#Declarar una Edad
+edad = 18
+
+print("Tengo",edad)
+
+#2 variablen en un codigo
+print("hola mi nombre es" + nombre)
+
+print("Hola mi nombre es" + nombre + "y tengo" + " " + str(edad) + "años")
+
+#Variable nombre(Mutable)
+nombre = "Matias"
+print("hola mi nuevo nombre es",nombre)
+
+#Utilizar la intruccion input
+nombre1 = input("¿Cual es tu nombre?")
+
+#Utilizar la intruccion input
+nombre1 = input("¿Cual es tu nombre?\n")
+print("Tu nombre es",nombre1)
--- 02-tipos de datos.py
+++ 02-tipos de datos.py
@@ -1,28 +1,50 @@
 #01-DATOS DE TIPO NUMERICO}
+edad = 18
 estatura = 1.71
 peso = 70
 conplejo = 1 -4j
 
+print("######## 01-DATOS NUM ########")
 print("Impresion del numero complejo:",conplejo)
 #OPERACIO ARITMETICA BÁSICA
 inc = peso/estatura**2
-print(inc)
+print("mi INC es de",inc)
+
+print("Mi INC es de: {:.2f}",format(inc),"\n")
 
 #02- DATOS DE TIPO CADENA DE CARACTERES
 asignatura = "Programacion"
 carrera = "Ingenieria Civil en Informatica"
-
+print("######## 02-STRING ########")
 print("La asignatura de",asignatura,"corresponde a la carrera de",carrera)
 
 #03- DATOS DE TIPO BOOLEAMOS
+print("######## 03-BOOLEAMOS ########")
 ampolleta = False
 iterruptor = True
 
 #Con-type savemos el tipo de datos que estamos tratando
 print(type(ampolleta))
 
-#04- DATOS TIPO ARRAY (Objetos de Tipo Colección)
+#04- DATOS TIPO LIST
+print("######## 04-LISTA ########")
+
+nueva_lista = list()
+print("Esta es otra lista vacia",otra_lista)
+print(type(otra_lista))
+
+#DECLARACION OTRA LISTA
 estudiantes = ["Matias", "Marcos", "Cristobal", "Sebastian"]
 num = [1,2,3,4,5,6]
-print(estudiantes)
-print(num)
+lenguaje = ["Python"]
+
+#¿Se puede realizar una lista de datos compuestos?
+data = ['Osorno',{'UV': 'nivel bajo'. 'Temp °C'; 15}, (-40.5725, -73.1353)]
+
+print("lista de cadena de caracteres:",estudiantes)
+print("lista de numeros",num)
+print("lista de un elemento",lenguaje)
+print("esto igual es una lista",data)
+print(len(lenguaje))
+print(estudianes.count("Marco"))
+
